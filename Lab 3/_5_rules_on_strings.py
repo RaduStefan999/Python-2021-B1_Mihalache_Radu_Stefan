88da0309e05c463ef4c4600d03a191b101b7cea2
Radu Stefan MIHALACHE <rmihalache@ias.bitdefender.biz>
2021-06-24 10:41:13 +0300

    problema 5 aproape gata

---
+++ Lab 3/_5_rules_on_strings.py
@@ -0,0 +1,25 @@
+def aply_rule(rule, string) :
+    return True
+
+def validate_rules (rules, strings) :
+
+    rules_status = True
+
+    for key in strings :
+        
+        OK = False
+
+        for tuplu in rules :
+            if (tuplu[0] == key) :
+                OK = True
+                rules_status = rules_status and aply_rule(tuplu, d[key])
+
+        if OK == False : return False
+
+    return rules_status
+
+
+s = {("key1", "", "inside", ""), ("key2", "start", "middle", "winter")}
+d = {"key1": "come inside, it's too cold out", "key3": "this is not valid"}
+
+print (validate_rules(s, d))
